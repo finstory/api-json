ddd416bd5fa224792af756db012509dd52ac7e26
finstory <facu995electro@hotmail.com>
2022-11-08 17:28:31 -0300

    s

--- db.js
+++ db.js
@@ -3,10 +3,12 @@ const cart = require("./data/cart");
 const orders = require("./data/orders");
 const users = require("./data/users");
 const categories = require("./data/categories");
+const favorites = require("./data/favorites");
 module.exports = {
   mugs,
   cart,
   orders,
   users,
-  categories
+  categories,
+  favorites,
 }
